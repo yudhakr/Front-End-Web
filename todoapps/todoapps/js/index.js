b1ccb201039a26bbb70cb813f17e21c066876bb7
yudhakr <yudhadeux@gmail.com>
2022-08-26 14:06:12 +0800

    ceck icon

--- todoapps/todoapps/js/index.js
+++ todoapps/todoapps/js/index.js
@@ -1,4 +1,15 @@
-function alert (string) {
-   console.log(string);
- }
- alert('Dicoding Indonesia');
+const language = 'Japanese';
+let greeting = '';
+switch (language) {
+  case 'English':
+    greeting = 'Good Morning!';
+    break;
+  case 'French':
+    greeting = 'Bonjour!';
+    break;
+  case 'Japanese':
+    greeting = 'Ohayou Gozaimasu!';
+  default:
+    greeting = 'Selamat Pagi!';
+}
+console.log(greeting);
