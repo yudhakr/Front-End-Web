760815dea7384974754d46fdf9aa04a567f26d81
yudhakr <yudhadeux@gmail.com>
2022-08-26 12:39:44 +0800

    style 1

---
+++ Booksheft Apps/js/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+   const submitForm = document.getElementById("form");
+
+   submitForm.addEventListener("submit", function (event) {
+       event.preventDefault();
+       addTodo();
+   });
+   const submitForm2 = document.getElementById("form2");
+
+   submitForm2.addEventListener("submit", function (event) {
+       event.preventDefault();
+       searchbook();
+   });
+
+   if(isStorageExist()){
+       loadDataFromStorage();
+   }
+});
+
+document.addEventListener("ondatasaved", () => {
+   console.log("Data berhasil di simpan.");
+});
+
+document.addEventListener("ondataloaded", () => {
+   refreshDataFromTodos();
+});
